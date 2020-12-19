ae9dedb46a4b2d828235a6a07023a5a48e5df992
danvelescu <velescudan98@gmail.com>
2020-12-19 16:27:46 +0200

    com1

--- project1.js
+++ project1.js
@@ -1,4 +1,12 @@
-function prindMyname(){
-   var divForText = document.getElementById('forText');
-   divForText.innerHTML += "<p>Hello World</p>";
+
+while (true) {
+  document.getElementById('forText').innerHTML += " "+x;
 }
+var list = [1,2,3,4,5];
+for(i = 0;i<10;i++){
+  list[i] = 0;
+}
+
+forEach((item, list) => {
+  intem = 0;
+});
